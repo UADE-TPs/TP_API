5eb893b7b04563e4e0ab5358a64421f97e0f870d
carlosvalentin <carlos.cagnani@gmail.com>
2018-05-27 20:01:35 -0300

    algunas clases peladas:)

--- src/Vista/PantallaAgregaParticipanteLista.java
+++ src/Vista/PantallaAgregaParticipanteLista.java
@@ -36,7 +36,7 @@ public class PantallaAgregaParticipanteLista extends JFrame {
 	 */
 	public PantallaAgregaParticipanteLista() {
 		setTitle("Nuevo participante lista");
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 		setBounds(100, 100, 450, 235);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
