c0435a0859d46b0ebf30a85d2b86956b7ba578c1
carlosvalentin <carlos.cagnani@gmail.com>
2018-05-27 20:03:41 -0300

    otras

---
+++ src/Controlador/AdmLista.java
@@ -0,0 +1,19 @@
+package Controlador;
+
+public class AdmLista {
+/*
+	generarLista(Ususario a, float m, Date fi, Date ff, String n, Date fa, String e): Lista
+	obtenerLista(int codLista): Lista
+	eliminarLista(Lista l): int
+	modificarMontoARecaudar(Lista l, float m): int
+	agregarPagoAParticLista(int codLista, Usuario u, Pago p): int
+	modificarFchInicio(Lista l, Date f): int
+	modificarNombAgasajado(Lista l, String n): int
+	modificarFchAgasajo(Lista l, Date f): int
+	modificarmailAgasajado(Lista l, String m): int
+	
+	
+	
+*/
+	
+}
---
+++ src/Modelo/Lista.java
@@ -0,0 +1,25 @@
+package Modelo;
+
+public class Lista {
+/*
+ * codLista: int;
+ * admin: Usuario;
+ * itemsLista: Collection;
+ * montoARecaudar: float;
+ * fechaInicio: Date;
+ * fechaFin: Date;
+ * nombreAgasajado: String;
+ * fechaAgasajo: Date;
+ * mailAgasajado: String;
+ * estadoLista: Char;
+ * 
+ * Metodos
+ * agregarParticipante(Usuario p): int
+ * agregarPago(Usuario u, Pago p): int
+ * obtenerMontoRecaudado(): float
+ * 
+ * 
+ * 
+ * 
+ */
+}
