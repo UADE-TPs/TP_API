c151daff2d2a86901bb49adeb624d0bd88dd991a
carlosvalentin <39595664+carlosvalentin@users.noreply.github.com>
2018-06-02 15:11:18 -0300

    Add files via upload

--- src/Modelo/ItemLista.java
+++ src/Modelo/ItemLista.java
@@ -1,5 +1,20 @@
 package Modelo;
 
 public class ItemLista {
+	
+	private Usuario particiante;
+	private float pago;
+	public ItemLista(Usuario participante, int pago) {
+		this.particiante=participante;
+		this.pago=pago;
+	}
+
+//Getters
+	public Usuario getParticipante() {
+	return this.particiante;
+}
+public float getPago() {
+	return this.pago;
+}
 
 }
--- src/Modelo/Lista.java
+++ src/Modelo/Lista.java
@@ -1,7 +1,80 @@
 package Modelo;
 
+import java.util.Vector;
+
+import Persistencia.MapperLista;
+
+import java.sql.Date;
+
 public class Lista {
-/*
+	
+	private int codLista;
+	private Usuario admin;
+	private Vector<ItemLista> itemsLista;
+	private float montoARecaudar;
+	private Date fechaInicio;
+	private Date fechaFin;
+	private String nombreAgasajado;
+	private Date fechaAgasajo;
+	private String mailAgasajado;
+	private boolean estadoLista;
+	//			l = new Lista(c,u,m,f1,ff,n,fa,e);
+	public Lista (int codlista , Usuario admin, float montoARecaudar,
+			Date fechaInicio, Date fechaFin, String nombreAgasajado,
+			Date fechaAgasajo, String mailAgasajado) {
+		super();
+		this.codLista=codlista;
+		this.admin=admin;
+		this.montoARecaudar=montoARecaudar;
+		this.fechaInicio=fechaInicio;
+		this.fechaFin=fechaFin;
+		this.fechaAgasajo=fechaAgasajo;
+		this.nombreAgasajado=nombreAgasajado;
+		this.mailAgasajado=mailAgasajado;
+		this.estadoLista=true;
+		this.itemsLista=null;
+		MapperLista.getInstancia().insert(this);
+		//this.itemsLista =null;
+		
+	
+	}	
+	public void bajaLista() {
+		this.estadoLista=false;
+		//return MapperLista.getInstancia().bajaLista(this.codLista);
+	}
+	//Getters
+	public int getCodigo() {
+		return codLista;
+	}
+	public Usuario getAdmin() {
+		return admin;
+	}
+	public float getMonto() {
+		return montoARecaudar;
+	}
+	public Date getfechaInicio() {
+		return fechaInicio;
+	}
+	public Date getFechaFin() {
+		return fechaFin;
+	}
+	public Date getFechaAgasajo() {
+		return fechaAgasajo;
+	}
+	public String getNombreAgasajado() {
+		return nombreAgasajado;
+	}
+	public String getMailAgasajado() {
+		return mailAgasajado;
+	}
+	public boolean getEstadoLista() {
+		return true;
+	}
+	public ItemLista getItemLista() {
+		return null;
+	}
+	
+	/*
  * codLista: int;
  * admin: Usuario;
  * itemsLista: Collection;
@@ -22,4 +95,5 @@ public class Lista {
  * 
  * 
  */
+
 }
